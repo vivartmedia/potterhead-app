35b50591de1c00f78aeb06b743374576305189c8
vivartmedia <nanno70@yahoo.de>
2024-06-21 16:38:46 -0700

    Refactor potterheadService to use axios for API requests

--- src/services/potterheadService.ts
+++ src/services/potterheadService.ts
@@ -4,8 +4,6 @@ import { Character } from '../../Interfaces/Interfaces';
 
 const API_URL = 'https://potterhead-api.vercel.app/api/characters';
 
-
-
 export const getCharacters = async (): Promise<Character[]> => {
   try {
     const response = await axios.get(API_URL);
